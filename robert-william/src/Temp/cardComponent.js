40875ef5e597163dc433e9946d57bc01f903e87d
Robert Keller <Kellerwrobert@gmail.com>
2018-03-30 13:11:04 -0400

    Completed card component
    1.) Added axios call to get data from public github api
    2.) Adds array of cards that you can add as a new component

--- robert-william/src/Temp/cardComponent.js
+++ robert-william/src/Temp/cardComponent.js
@@ -1,9 +1,9 @@
 const Card = (props) => {
   return(
-  	<div style={{color: '#4da6ff'}}>
-    	<img width="75" src={props.avatar_url} />
+  	<div style={{marginTop: 10}}>
+    	<img width="100" src={props.avatar_url} />
       <div style={{display: "inline-block", marginLeft: 10}}>
-      	<div>{props.name}</div>
+      	<div style={{fontSize: 20, fontWeight: "bold"}}>{props.name}</div>
       	<div>{props.company}</div>
       </div>
   	</div>
@@ -11,14 +11,58 @@ const Card = (props) => {
 }
 
 const CardList = (props) => {
+
 	return(
   	<div>
-    	<Card name="Paul O’Shannessy"
-        avatar_url="https://avatars1.githubusercontent.com/u/8445?v=4"
-        company="Facebook"
-      />
+    	{props.cards.map((card) => <Card key={card.id} {...card} />)}
     </div>
   );
 }
 
-ReactDOM.render(<CardList />, mountNode);
+class Form extends React.Component {
+
+  state = { userName: '' }
+  handleSubmit = (event) => {
+  	event.preventDefault();
+    axios.get(`https://api.github.com/users/${this.state.userName}`)
+    	.then(resp => {
+      	this.props.onSumbit(resp.data);
+        this.setState({userName: ''});
+      });
+  };
+
+  render() {
+    return(
+    	<form onSubmit={this.handleSubmit}>
+      	<input type="text"
+        	value={this.state.userName}
+          onChange={(event) => this.setState({ userName: event.target.value})}
+          placeholder="Github username" required />
+        <button type="submit">Add Card</button>
+      </form>
+    );
+  }
+}
+
+class App extends React.Component {
+	state = {
+  	cards: []
+  };
+
+  addNewCard = (cardInfo) => {
+  	this.setState(prevState => ({
+    	cards: prevState.cards.concat(cardInfo)
+    }))
+  };
+
+	render() {
+  	return(
+      <div>
+        <Form onSumbit={this.addNewCard}/>
+        <CardList cards={this.state.cards} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, mountNode);
